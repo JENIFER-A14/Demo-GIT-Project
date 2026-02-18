4eabb0b82545bc96795719c777279e3d9ab11c6e
JENIFER-A14 <jenifersuhana@gmail.com>
2026-02-18 09:51:11 +0530

    Tester-2 Script

---
+++ src/test/java/com/sample/GitClass3.java
@@ -0,0 +1,11 @@
+package com.sample;
+
+public class GitClass3 {
+ private void meth1() {
+	
+}
+ private void meth2() {
+		
+}
+
+}
---
+++ src/test/java/com/sample/GitClass4.java
@@ -0,0 +1,12 @@
+package com.sample;
+
+public class GitClass4 {
+	private void sample1() {
+		
+	}
+private void sample2() {
+		
+	}
+
+
+}
