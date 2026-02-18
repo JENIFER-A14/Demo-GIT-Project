4bda27d35fe5006a8ffbcdc97bd36a589e299514
JENIFER-A14 <jenifersuhana@gmail.com>
2026-02-18 09:32:22 +0530

    Tester-1 Script

--- src/test/java/com/sample/GitClass1.java
+++ src/test/java/com/sample/GitClass1.java
@@ -1,5 +1,29 @@
 package com.sample;
 
 public class GitClass1 {
+	private void method1() {
+		System.out.println("1");
+	}
+
+	private void method2() {
+		System.out.println("2");
+	}
+
+	private void method3() {
+		System.out.println("3");
+	}
+	
+	private void method4() {
+		System.out.println("4");
+	}
+	
+	private void method5() {
+		System.out.println("5");
+	}
+	
+	private void method6() {
+		System.out.println("6");
+	}
+	
 
 }
---
+++ src/test/java/com/sample/GitClass2.java
@@ -0,0 +1,15 @@
+package com.sample;
+
+public class GitClass2 {
+	private void webAppTest() {
+		System.out.println("11");
+	}
+	private void apiTest() {
+		System.out.println("12");
+	}
+	private void backEndTest() {
+		System.out.println("13");
+
+	}
+
+}
